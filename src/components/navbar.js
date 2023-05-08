4e7518f71408c93db6bb1e663a1cbb10775068d7
jacobgervin <jacobgervin@gmail.com>
2023-05-08 20:41:55 +0200

    Create react app + tailwind

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+      <div>
+        <a>JACOB GERVIN</a>
+        <ul>
+            <li><a>WORK</a></li>
+            <li><a>ABOUT</a></li>
+            <li><a>CONTACT</a></li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
